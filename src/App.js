edf81d5a8c829919fe79e6c9f1fb7e153381a5ec
Paparoach0309 <silek88@mail.ru>
2022-03-11 15:37:56 +0300

    complete redux app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash);
+    console.log(cash);
+
+    const addCount = () => {
+        dispatch({type:"ADD_COUNT", payload: 1});
+    }
+
+    const decCount = () => {
+        dispatch({type:"DEC_COUNT", payload: 1});
+    }
+
+    const reset = () => {
+        dispatch({type:"RESET"});
+    };
+
+	return (
+		<div className="app">
+			<div className="counter">{cash}</div>
+            <div className= "even-number" style={(cash % 2 === 0 ? {background:'red'} : {background:'white'})}>Введено четное число</div>
+            <div className="counter-buttons">
+                <button className="counter-btn" onClick={decCount}>-</button>
+                <button className="counter-btn" onClick={reset}>Reset</button>
+                <button className="counter-btn" onClick={addCount}>+</button>
+            </div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaltState = {
+  cash: 0,
+}
+const reducer =  (state = defaltState, action) => {
+  switch (action.type) {
+    case "ADD_COUNT":
+      return {...state, cash: state.cash + action.payload}
+    
+    case "DEC_COUNT":
+      return {...state, cash: state.cash - action.payload}
+
+    case "RESET":
+      return {...state, cash: 0}
+  
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
